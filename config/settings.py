2d3579c1b7d784b94a069ad10e0bf8d24847ed2e
Welder Rodrigues de Medeiros <73802580+welder60@users.noreply.github.com>
2026-06-05 12:12:39 -0300

    Adiciona ALLOWED_HOSTS e origem confiável CSRF
    adiciona o site 'https://sigesporte.duat.site' como host permitido.

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'sigesporte.duat.site','localhost', '127.0.0.1'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://sigesporte.duat.site',
+]
 
 
 # Application definition
