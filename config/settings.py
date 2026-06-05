8c98b08f6373c892aea9966bb4a1e6f729fda463
Welder Rodrigues de Medeiros <73802580+welder60@users.noreply.github.com>
2026-06-05 15:35:52 -0300

    Descomenta STATIC_URL para permitir o collectstatic

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 # ajustado para collectstatic funcionar
-#STATIC_URL = "static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
