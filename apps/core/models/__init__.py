1ac1d7c22db1199a5830fefd86a1d5db6ec78ffe
Davi Gualberto Rocha <davigualbertorocha1@gmail.com>
2026-06-08 20:51:43 -0300

    feat: implementa inscricao e cancelamento em eventos (US-007 e US-009)

--- apps/core/models/__init__.py
+++ apps/core/models/__init__.py
@@ -8,13 +8,15 @@ Exporta
 - Evento
 - EspacoFisico
 - Organizacao
-
+- Inscricao
 
 - Criado por `Welder60 <https://github.com/welder60>`_ em 01 junho 2026
+- Alterado por DaviiGualbertoo <https://github.com/DaviiGualbertoo>`_ em 08 junho 2026
 """
 
 from apps.core.models.eventos_models import Evento
 from apps.core.models.espacos_models import EspacoFisico
 from apps.core.models.organizacoes_models import Organizacao
+from apps.core.models.inscricao_models import Inscricao
 
-__all__ = ["Evento", "EspacoFisico", "Organizacao"]
+__all__ = ["Evento", "EspacoFisico", "Organizacao", "Inscricao"]
