2e04486265a476b1efbb94c6d5ccb50b8c816834
Welder Rodrigues de Medeiros <73802580+welder60@users.noreply.github.com>
2026-06-08 14:35:51 -0300

    Atualiza formulário de cadastro

--- apps/security/urls.py
+++ apps/security/urls.py
@@ -11,6 +11,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
+from apps.security.controllers.cadastro_controller import cadastro
 from apps.security.controllers.area_restrita_controller import (
     area_restrita_redirect,
     perfil,
@@ -31,12 +32,7 @@ urlpatterns = [
     ),
     # Rota de Logout
     path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
-    # Rota temporária de Cadastro (Apenas para não dar erro no botão)
-    path(
-        "cadastro/",
-        TemplateView.as_view(template_name="security/cadastro.html"),
-        name="cadastro",
-    ),
+    path("cadastro/", cadastro, name="cadastro"),
     # Rota de recuperação de senha
     path(
         "esqueci-senha/",
